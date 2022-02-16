a757e57b4fb074a084ea38beb8a7cc0a9655fe6d
DCT <mirachan2332@gmail.com>
2022-02-15 18:06:03 -0600

    working with inquirer, fixed error with connection and console.table

--- db/index.js
+++ db/index.js
@@ -5,6 +5,18 @@ class DB {
     constructor (connection){
         this.connection = connection;
     }
+
+    //build query for each database call
+
+    findDepartments(){
+        return new Promise((resolve, reject) => {
+            this.connection.query('SELECT * FROM department', (error, data) => {
+                resolve(data);
+            })
+        })
+
+    }
+
 }
 
 module.exports = new DB(connection);
--- index.js
+++ index.js
@@ -1,3 +1,67 @@
 const inquirer = require('inquirer');
 const consoletable = require('console.table');
 const mySQL = require('mysql2');
+const DB = require('./db');
+const db = require('./db');
+
+//create a directory
+// what would you like to do?
+// * view employees
+// * view department
+// * view roles
+
+// View employees
+// *Add
+// *edit
+// *delete
+
+
+function directory () {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'directoryChoice',
+            message: 'What would you like to do?',
+            choices: [
+                {
+                    name: 'View Employees',
+                    value: 'view_employees'
+                },
+                {
+                    name: 'View Departments',
+                    value: 'view_departments'
+                },
+                {
+                    name: 'Quit',
+                    value: 'quit'
+                }
+            ]
+        }
+    ]).then((response) => {
+        switch (response.directoryChoice){
+            case 'view_employees':
+                viewEmployees();
+                break;
+            case 'view_departments':
+                viewDepartments();
+                break;
+            default:
+                quit();
+        }
+    })
+}
+
+
+function viewDepartments(){
+    db.findDepartments()
+    .then(([departments]) => {
+        console.table(departments);
+    })
+}
+function viewEmployees() {}
+
+function quit() {
+
+}
+
+directory();
